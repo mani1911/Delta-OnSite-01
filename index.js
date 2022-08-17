7dc1be29301d23d53862093e065e8e4ae98bf661
mani1911 <mail2aj19@gmail.com>
2022-08-17 23:13:40 +0530

    added drag feature

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const incBtn = document.querySelector('.inc');
 const decBtn = document.querySelector('.dec');
 const display = document.querySelector('.layers');
 const selColor = document.querySelector('.selectColor');
+const draggable = document.querySelector('.draggable');
 const main = document.querySelector('.main');
 
 layer5.style.display = "none";
@@ -19,6 +20,10 @@ let val = 0;
 display.innerHTML = val;
 
 incBtn.addEventListener('click',()=>{
+    if(val >=5){
+        alert('Maximum Limit Reached');
+        return;
+    }
     if(val<5){
         val++;
         display.innerHTML = val;
@@ -53,6 +58,10 @@ incBtn.addEventListener('click',()=>{
 })
 
 decBtn.addEventListener('click', ()=>{
+    if(val <=0){
+        alert('No Layers Left');
+        return;
+    }
     if(val>0){
         val--;
         display.innerHTML = val;
@@ -79,5 +88,48 @@ decBtn.addEventListener('click', ()=>{
             
     }
 })
+
+
+const dragElement = (element, dragzone) =>{
+    let pos1 = 0, pos2 =0, pos3=0, pos4=0;
+    const dragMouseUp = ()=>{
+        document.onmouseup = null;
+        document.onmousemove = null;
+        element.classList.remove("drag");
+
+    }
+
+    const dragMouseMove = (event)=>{
+        event.preventDefault();
+        pos1 = pos3 - event.clientX;
+        pos2 = pos4 - event.clientY;
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+
+        element.style.top = `${element.offsetTop - pos2}px`;
+        element.style.left = `${element.offsetLeft- pos1}px`;
+
+    }
+
+    const dragMouseDown = (event)=>{
+        event.preventDefault();
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+
+        element.classList.add("drag");
+
+        document.onmouseup = dragMouseUp;
+        document.onmousemove = dragMouseMove;
+
+
+    }
+
+    dragzone.onmousedown = dragMouseDown;
+
+};
+
+dragElement(draggable, main)
+
+
 
 
