205fd7f44e869a8e9d3dbc7761b34d659d3fb7f1
mani1911 <mail2aj19@gmail.com>
2022-08-17 18:42:56 +0530

    created task

---
+++ index.js
@@ -0,0 +1,74 @@
+const layer5 = document.querySelector('.layer5');
+const layer4 = document.querySelector('.layer4');
+const layer3 = document.querySelector('.layer3');
+const layer2 = document.querySelector('.layer2');
+const layer1 = document.querySelector('.layer1');
+const incBtn = document.querySelector('.inc');
+const decBtn = document.querySelector('.dec');
+const display = document.querySelector('.layers');
+
+layer5.style.display = "none";
+layer4.style.display = "none";
+layer3.style.display = "none";
+layer2.style.display = "none";
+layer1.style.display = "none";
+
+let val = 0;
+display.innerHTML = val;
+
+incBtn.addEventListener('click',()=>{
+    if(val<5){
+        val++;
+        display.innerHTML = val;
+        console.log(val)
+    }
+    switch(val){
+        case 1:
+            layer5.style.display = "block";
+            break;
+        case 2:
+            layer4.style.display = "block";
+            break;
+        case 3:
+            layer3.style.display = "block";
+            break;
+        case 4:
+            layer2.style.display = "block";
+            break;
+        case 5:
+            layer1.style.display = "block";
+            break;
+        default:
+            break;
+            
+    }
+
+})
+
+decBtn.addEventListener('click', ()=>{
+    if(val>0){
+        val--;
+        display.innerHTML = val;
+        console.log(val)
+    }
+    switch(val){
+        case 0:
+            layer5.style.display = "none";
+            break;
+        case 1:
+            layer4.style.display = "none";
+            break;
+        case 2:
+            layer3.style.display = "none";
+            break;
+        case 3:
+            layer2.style.display = "none";
+            break;
+        case 4:
+            layer1.style.display = "none";
+            break;
+        default:
+            break;
+            
+    }
+})
